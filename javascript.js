d4d697a80a62c82eb8ba1877d06553fc2d766c8c
Connor Sheets <connorsheets32@gmail.com>
2023-02-13 17:09:43 -0600

    newQuestion

--- javascript.js
+++ javascript.js
@@ -9,36 +9,53 @@ const submitButton = document.createElement("button")
 //      dogs.forEach(dog => )
 //     }
 
-const yesButton = document.getElementById("yes-button")
-yesButton.addEventListener("click", () => {
-  document.getElementById('questions-1').style.display = 'none'
-  const form1Div = document.createElement("div")
+function addNewQ (){
+    let form1Div = document.createElement("div")
   form1Div.className = "form-group"
+  form1Div.id = "form-1"
   const form1 = document.createElement("form")
-  form1.id = "form1"
-  const input01 = document.createElement("input")
-  input01.type = "radio"
-  input01.name = "choice"
-  input01.value = "yes"
-  input01.textContent = "Yes"
-
-  const input02 = document.createElement("input")
-  input02.type = "radio"
-  input02.name = "choice"
-  input02.value = "no"
-  input02.innerText = "No"
-  
-  submitButton.innerText = "Submit Answer"
-  form1.append(input01, input02);
-  form1Div.append(form1)
+  form1.innerHTML = `
+    <h1> New Question</h1><br>
+     <form>
+      <input type="radio" name="choice" value="yes"> Yes
+     <input type="radio" name="choice" value="no"> No
+    </form>
+  `
+  form1Div.append(form1);
   const body = document.getElementsByTagName("body")[0]
   body.append(form1Div)
-
   const question1 = document.createElement("h2")
   question1.innerText = "Do you have a dog?"
   form1Div.append(question1)
+}
 
-})
+const yesButton = document.getElementById("yes-button")
+yesButton.addEventListener("click", console.log("hi"))
+function addSubmitBtn() {
+    document.getElementById('questions-1').style.display = 'none'
+    let submitBtn = document.createElement("button")
+    submitBtn.id = "submit1"
+    submitBtn.innerText = "Submit"
+    submitBtn.addEventListener("click", () =>{
+    console.log("Submit")
+  })
+}
+
+
+//   const input02 = document.createElement("input")
+//   input02.type = "radio"
+//   input02.name = "choice"
+//   input02.value = "no"
+//   input02.innerText = "No"
+//   form1.append(input01);
+  
+
+  
+
+
+
+let submit1 = document.querySelector("#submit1")
+console.log(submit1)
 
 const noButton = document.getElementById("no-button")
 
