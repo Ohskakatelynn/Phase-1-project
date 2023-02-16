97fd60d7b71d93a6f2a2195963fcdf9fc343a740
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-02-15 20:53:10 -0500

    event listener

--- javascript.js
+++ javascript.js
@@ -1,9 +1,13 @@
 let showForm = false
 const yesButton = document.getElementById("yes-button")
+
 const question = document.getElementById("questionH2")
+const dogDiv = document.querySelector("#dog-div")
+
+
 yesButton.addEventListener("click", ()=> {
     const surveyDiv = document.getElementById("survey-div")
-    console.log("hi")
+    
     showForm = !showForm
     if(showForm) {
       formDiv.style.display = "block"
@@ -13,33 +17,110 @@ yesButton.addEventListener("click", ()=> {
     }
 })
 
-
+let ourDogs = []
 fetch ( "http://localhost:3000/dogs" )
     .then ( r=> r.json() )
-    .then (data => console.log(data))
+    .then (dogData => {
+      let dogArray = [...dogData]
+      ourDogs.push(dogArray)
+    })
   
-    
 const formDiv = document.querySelector("#form-div")
-const dogDiv = document.getElementById("dog-image")
 const questionForm = document.querySelector("#question-form")
 questionForm.addEventListener("submit", (e) => {
   e.preventDefault()
-  console.log(e.target.choice.value)
   
   if (e.target.choice.value == "yes") {
-    renderBigDogs()
+    getBigDogs()
   } else if (e.target.choice.value == "no"){
-    console.log( "small dogs" )
+    getNotBigDogs()
   } 
+  formDiv.style.display= "none"
 })
 
+function getDogs(dogs){
+  dogs.forEach(dog => {
+    let imgAndBreed = document.createElement("div");
+    imgAndBreed.id = dog.id
+    if(dog.size === 4){
+      imgAndBreed.className = "big"
+    } else if (dog.size < 4){
+      imgAndBreed.className = "not-big"
+    }
+    let dogImg = document.createElement("img")
+    dogImg.src = dog.image
+    dogImg.className = "dog-image"
+    let breedName = document.createElement("p")
+    breedName.innerText = dog.breed
+    dogDiv.append(imgAndBreed)
+    imgAndBreed.append(breedName, dogImg)
+
+    dogImg.addEventListener("click", () => {
+      const list = document.getElementById("ul")
+      const itemsOnList = document.createElement("li")
+      itemsOnList.innerText = dog["energy-level"]
+      itemsOnList.innerText = dog.price
+      itemsOnList.innerText = dog.friendly
+      itemsOnList.innerText = dog.sheds
+      itemsOnList.innerText = dog.sassy
+      list.append(itemsOnList)
+      dogImg.appendChild(list)
+    
+    // dogImg.addEventListener("mouseover", () => {
+    //   alert("Give dog pets")
+    // })
+    // let breedName = document.createElement("p")
+    // breedName.innerText = dog.breed
+    // dogDiv.append(imgAndBreed)
+    // imgAndBreed.append(breedName, dogImg)
+
+    // breedName.addEventListener("click", () => {
+    //   const list = document.getElementById("ul")
+    //   const itemsOnList = document.createElement("li")
+    //   itemsOnList.innerText = dog["energy-level"]
+    //   itemsOnList.innerText = dog.price
+    //   itemsOnList.innerText = dog.friendly
+    //   itemsOnList.innerText = dog.sheds
+    //   itemsOnList.innerText = dog.sassy
+    //   list.append(itemsOnList)
+    //   breedName.append(list)
+    //   })
+    })
+  }
+  )}
 
 
- function renderBigDogs(dogs) {
-   
+function getBigDogs() {
+  fetch("http://localhost:3000/dogs")
+  .then(response => response.json())
+  .then(data =>{
+    const largeDogs = data.filter(dog => dog.size === 4)
+    getDogs(largeDogs)
+    let showBigDogs = true
+    if (showBigDogs) {
+      const notBigDogs = document.querySelectorAll(".not-big")
+      for (let i = 0; i < notBigDogs.length; i++) {
+        notBigDogs[i].style.display = "none";
+      }
+    }
+  })
+}
+ function getNotBigDogs(){
+    fetch("http://localhost:3000/dogs")
+    .then(response => response.json())
+    .then(data =>{
+      const notBigDogs = data.filter(dog => dog.size < 4)
+      getDogs(notBigDogs)
+      let showBigDogs = true
+    if (showBigDogs) {
+      const bigDogs = document.querySelectorAll(".big")
+      for (let i = 0; i < bigDogs.length; i++) {
+        bigDogs[i].style.display = "none";
+      }
+    }
+  })
  }
-
-
+ 
   
 
 
